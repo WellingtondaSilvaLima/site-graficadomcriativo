3e942c001a30b4fe40116f6d0dbcf41a6aaef9fb
Wellington Lima <wellingtonlima.jt@gmail.com>
2024-09-12 09:33:30 -0300

    Adicionado animações

---
+++ index.js
@@ -0,0 +1,125 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-a-dom');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-criatividades');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-call-to-action');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-os-criativos');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-os-criativos-1');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-os-criativos-2');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-os-criativos-3');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-os-criativos-4');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const target = document.querySelector('.animacao-os-criativos-5');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        target.classList.add('visivel');
+      }
+    });
+  }, { threshold: 0.03 }); // Ajuste o threshold conforme necessário
+
+  observer.observe(target);
+});
